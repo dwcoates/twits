913f02696518311eb806489e0c62a37994529d35
dwcoates <dodge.w.coates@gmail.com>
2017-03-14 17:59:53 -0400

    Get preprocess working

--- preprocess.py
+++ preprocess.py
@@ -5,12 +5,43 @@ Preprocess json files.
 """
 
 import logging
+import sys
+import os
 
 import json
 import gzip
-import io
+import csv
 
 logging.basicConfig(filename="preprocess.log", level=logging.DEBUG)
+
+# feature csv names with corresponding json paths
+FEATURES = {"id_str":                    lambda d: d["id_str"],
+            "retweeted":                 lambda d: d["retweeted"],
+            "created_at":                lambda d: d["created_at"],
+            "user_id_str":               lambda d: d["user"]["id_str"],
+            "user_friends_count":        lambda d: d["user"]["friends_count"],
+            "user_lang":                 lambda d: d["user"]["lang"],
+            "user_profile_text_color":   lambda d: d["user"]["profile_text_color"],
+            "user_favourites_count":     lambda d: d["user"]["favourites_count"],
+            "user_description":          lambda d: d["user"]["description"],
+            "user_statuses_count":       lambda d: d["user"]["statuses_count"],
+            "user_contributors_enabled": lambda d: d["user"]["contributors_enabled"],
+            "user_followers_count":      lambda d: d["user"]["followers_count"],
+            "user_screen_name":          lambda d: d["user"]["screen_name"],
+            "user_verified":             lambda d: d["user"]["verified"],
+            "user_notifications":        lambda d: d["user"]["notifications"],
+            "user_listed_count":         lambda d: d["user"]["listed_count"],
+            "user_created_at":           lambda d: d["user"]["created_at"],
+            "user_name":                 lambda d: d["user"]["name"],
+            "user_following":            lambda d: d["user"]["following"],
+            "retweet_count":             lambda d: d["retweet_count"],
+            "text":                      lambda d: d["text"],
+            "entities_hashtags":         lambda d: d["entities"]["hashtags"],
+            "entities_urls":             lambda d: d["entities"]["urls"],
+            "entities_user_mentions":    lambda d: d["entities"]["user_mentions"],
+            "favorited":                 lambda d: d["favorited"],
+            "created_at":                lambda d: d["created_at"],
+            "contributors":              lambda d: d["contributors"]}
 
 def read_json(filename, gz=None):
     """
@@ -49,89 +80,81 @@ def read_json(filename, gz=None):
 
     return data
 
+def csvify_json_obj(jobj):
+    """
+    Return the json object ``jobj`` flattened into a line of csv
+    """
+    return map(lambda p: p(jobj), FEATURES.values())
 
-def process_json(filename):
+def process_json(filename, output_filename, gz=False):
     """
     Process json.
     """
-    attrs = ["total_fails, total_parses, total_deletes"]
-    hist = dict(zip(attrs, [0] * len(attrs)))
+
+    props = ["total_fails",
+             "total_parses",
+             "english_count",
+             "encode_exceptions",
+             "other_exceptions"]
+    history = dict(zip(props, [0] * len(props)))
 
     logging.info("Reading '{}'...".format(filename))
-    for i, j in enumerate(read_json(filename, gz=True)):
-        try:
-            if "delete" in j:
-                history["total_deletes"] += 1
-            elif j["user"]["lang"] is "en":
-                out = out_line(j["id_str"],
-                               j["retweeted"],
-                               j["created_at"],
-                               j["user"]["id_str"],
-                               j["user"]["friends_count"],
-                               j["user"]["lang"],
-                               j["user"]["profile_text_color"],
-                               j["user"]["favourites_count"],
-                               j["user"]["description"],
-                               j["user"]["statuses_count"],
-                               j["user"]["contributors_enabled"],
-                               j["user"]["followers_count"],
-                               j["user"]["screen_name"],
-                               j["user"]["verified"],
-                               j["user"]["notifications"],
-                               j["user"]["listed_count"],
-                               j["user"]["created_at"],
-                               j["user"]["name"],
-                               j["user"]["following"],
-                               j["retweet_count"],
-                               j["text"],
-                               j["entities"]["hashtags"],
-                               j["entities"]["urls"],
-                               j["entities"]["user_mentions"],
-                               j["favorited"],
-                               j["created_at"],
-                               j["contributors"],
-                               removeCommas(job["text"]),
-                               parseText(job["text"]))
-                history["english_count"] += 1
-                fout.write(out)
-        except UnicodeEncodeError as ex:
-            history["encode_exceptions"] += 1
-            logging.warning("Encoding exception: {}".format(ex.message))
-        except Exception:
-            history["other_exceptions"] += 1
-            logging.warning("Other exception: {}".format(ex.message))
 
-    logging.warning(
-        "Encode exceptions for '{}': {}".format(filename,
-                                                history["encode_exceptions"]))
-    logging.warning(
-        "Other exceptions for '{}': {}".format(filename,
-                                               history["encode_exceptions"])))
-    logging.info(
-        "Total deletes for '{}': {}".format(filename,
-                                            history["total_deletes"]))
+    with open(output_filename, 'rb') as fout:
+        tweet_writer = csv.writer(fout)
+        tweet_writer.writerow(FEATURES.keys()) # csv header
+        for i, j in enumerate(read_json(filename, gz=gz)):
+            try:
+                if "delete" in j:
+                    pass # ignore deletes for now
+                elif j["user"]["lang"] == "en":
+                    tweet_writer.writerow(csvify_json_obj(j))
+                    history["english_count"] += 1
+                else:
+                    pass # don't care about non-English tweets
+            except UnicodeEncodeError as ex:
+                history["encode_exceptions"] += 1
+                logging.warning("Encoding exception: {}".format(ex.message))
+            except KeyError as ex:
+                raise ex
+            except Exception as ex:
+                history["other_exceptions"] += 1
+                logging.error("Other exception: {}".format(ex.message))
 
-    fout.close()
-
+            logging.warning(
+                "Encode exceptions for '{}': {}".format(
+                    filename, history["encode_exceptions"]))
+            logging.warning(
+                "Other exceptions for '{}': {}".format(
+                    filename, history["encode_exceptions"]))
 
     logging.info("Finished reading '{}'...".format(filename))
 
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        raise ValueError(
+            "{} accepts 2 arguments, recieved {}".format(
+                __file__,
+                len(sys.argv) - 1))
 
+    dest_path = sys.argv[2]
+    data_path = sys.argv[1]
 
-logging.info("Clearning output dir...")
-clear_output_dir(dest_path) # remove data remnants
+    print os.listdir(data_path)
 
-dirs = listdir(origin_path)
+    logging.info("Clearning output dir...")
 
-# max?
-logging.info("Processing {} dirs...".format(len(dirs)))
-for f in dirs:
-    if max <= 0:
-        break
-    max -= 1
-    history["file_count"] += 1
+    clear_output_dir(dest_path) # remove data remnants
 
-    process_json(dest_path+f)
+    # dirs = listdir(origin_path)
 
-    fout = open(dest_path+f[0:-3], 'w')
-    fout.write(get_header())
+    # # max?
+    # logging.info("Processing {} dirs...".format(len(dirs)))
+    # for f in dirs:
+    #     if max <= 0:
+    #         break
+    #     max -= 1
+    #     history["file_count"] += 1
+    #     process_json(dest_path+f)
+    #     fout = open(dest_path+f[0:-3], 'w')
+    #     fout.write(get_header())
