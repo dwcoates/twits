720331e7552852dde76ae9d100c7db98b732c471
dwcoates <dodge.w.coates@gmail.com>
2017-03-14 00:59:42 -0400

    Add general preprocess function

--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,20 @@
 """
 Preprocess json files.
 """
+import logging
+
 import json
 import gzip
 import io
 
-import pandas as pd
-
+logging.basicConfig(filename="preprocess.log", level=logging.DEBUG)
 
 def read_json(filename, gz=None):
     """
     Read in comma-newline delimited json files encoded in latin-1.
     Use the gz flag to signify that the filename is a gzip file.
+
+    ``something``
     """
 
     _open = gzip.open if gz else open
@@ -43,19 +46,81 @@ def read_json(filename, gz=None):
 
     return data
 
-def read_pandas(filename, gz=None):
 
-    _io = gzip.io if gz else io
-
-    with _io.open(filename, encoding="latin1") as f:
-        s = f.read().split(",\n")
-
-        df = pd.read_json(s)
-
-    return df
-
-def process_json(j):
+def process_json(filename):
     """
-    Stuff.
+    Process json.
     """
-    pass
+    attrs = ["total_fails, total_parses, total_deletes"]
+    hist = dict(zip(attrs, [0] * len(attrs)))
+
+    logging.info("Reading '{}'...".format(filename))
+    for i, j in enumerate(read_json(filename, gz=True)):
+        try:
+            if "delete" in j:
+                history["total_deletes"] += 1
+            elif j["user"]["lang"] is not "en":
+                out = out_line(j["id_str"],
+                               j["retweeted"],
+                               j["created_at"],
+                               j["user"]["id_str"],
+                               j["user"]["friends_count"],
+                               j["user"]["lang"],
+                               j["user"]["profile_text_color"],
+                               j["user"]["favourites_count"],
+                               j["user"]["description"],
+                               j["user"]["statuses_count"],
+                               j["user"]["contributors_enabled"],
+                               j["user"]["followers_count"],
+                               j["user"]["screen_name"],
+                               j["user"]["verified"],
+                               j["user"]["notifications"],
+                               j["user"]["listed_count"],
+                               j["user"]["created_at"],
+                               j["user"]["name"],
+                               j["user"]["following"],
+                               j["retweet_count"],
+                               j["text"],
+                               j["entities"]["hashtags"],
+                               j["entities"]["urls"],
+                               j["entities"]["user_mentions"],
+                               j["favorited"],
+                               j["created_at"],
+                               j["contributors"],
+                               removeCommas(job["text"]),
+                               parseText(job["text"]))
+                history["english_count"] += 1
+                fout.write(out)
+        except UnicodeEncodeError as ex:
+            history["encode_exceptions"] += 1
+        except Exception as ex:
+            history["other_exceptions"] += 1
+
+    logging.warning(
+        "Encode exceptions for '{}': {}".format(filename,
+                                                history["encode_exceptions"]))
+    logging.warning(
+        "Other exceptions for '{}': {}".format(filename,
+                                               history["encode_exceptions"])))
+    logging.info(
+        "Total deletes for '{}': {}".format(filename,
+                                            history["total_deletes"]))
+    fout.close();
+
+    logging.info("Finished reading '{}'...".format(filename))
+
+clear_output_dir(dest_path) # remove data remnants
+
+dirs = listdir(origin_path)
+
+# max?
+for f in dirs:
+    if max <= 0:
+        break
+    max -= 1
+    history["file_count"] += 1
+
+    process_json(dest_path+f)
+
+    fout = open(dest_path+f[0:-3], 'w')
+    fout.write(get_header())
