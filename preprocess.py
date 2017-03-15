8be9775b7122f40aca0d6caec6ef1c81d8656284
dwcoates <dodge.w.coates@gmail.com>
2017-03-14 21:46:56 -0400

    Style corrections. Replace dictionary with tuple list for FEATURES

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python
 
+# pylint: disable=bad-whitespace,star-args
+
 """
 Preprocess json files.
 """
@@ -15,33 +17,35 @@ import csv
 logging.basicConfig(filename="preprocess.log", level=logging.DEBUG)
 
 # feature csv names with corresponding json paths
-FEATURES = {"id_str":                    lambda d: d["id_str"],
-            "retweeted":                 lambda d: d["retweeted"],
-            "created_at":                lambda d: d["created_at"],
-            "user_id_str":               lambda d: d["user"]["id_str"],
-            "user_friends_count":        lambda d: d["user"]["friends_count"],
-            "user_lang":                 lambda d: d["user"]["lang"],
-            "user_profile_text_color":   lambda d: d["user"]["profile_text_color"],
-            "user_favourites_count":     lambda d: d["user"]["favourites_count"],
-            "user_description":          lambda d: d["user"]["description"],
-            "user_statuses_count":       lambda d: d["user"]["statuses_count"],
-            "user_contributors_enabled": lambda d: d["user"]["contributors_enabled"],
-            "user_followers_count":      lambda d: d["user"]["followers_count"],
-            "user_screen_name":          lambda d: d["user"]["screen_name"],
-            "user_verified":             lambda d: d["user"]["verified"],
-            "user_notifications":        lambda d: d["user"]["notifications"],
-            "user_listed_count":         lambda d: d["user"]["listed_count"],
-            "user_created_at":           lambda d: d["user"]["created_at"],
-            "user_name":                 lambda d: d["user"]["name"],
-            "user_following":            lambda d: d["user"]["following"],
-            "retweet_count":             lambda d: d["retweet_count"],
-            "text":                      lambda d: d["text"],
-            "entities_hashtags":         lambda d: d["entities"]["hashtags"],
-            "entities_urls":             lambda d: d["entities"]["urls"],
-            "entities_user_mentions":    lambda d: d["entities"]["user_mentions"],
-            "favorited":                 lambda d: d["favorited"],
-            "created_at":                lambda d: d["created_at"],
-            "contributors":              lambda d: d["contributors"]}
+FEATURES = [("id_str",                    lambda d: d["id_str"]),
+            ("retweeted",                 lambda d: d["retweeted"]),
+            ("created_at",                lambda d: d["created_at"]),
+            ("user_id_str",               lambda d: d["user"]["id_str"]),
+            ("user_friends_count",        lambda d: d["user"]["friends_count"]),
+            ("user_lang",                 lambda d: d["user"]["lang"]),
+            ("user_profile_text_color",   lambda d: d["user"]["profile_text_color"]),
+            ("user_favourites_count",     lambda d: d["user"]["favourites_count"]),
+            ("user_description",          lambda d: d["user"]["description"]),
+            ("user_statuses_count",       lambda d: d["user"]["statuses_count"]),
+            ("user_contributors_enabled", lambda d: d["user"]["contributors_enabled"]),
+            ("user_followers_count",      lambda d: d["user"]["followers_count"]),
+            ("user_screen_name",          lambda d: d["user"]["screen_name"]),
+            ("user_verified",             lambda d: d["user"]["verified"]),
+            ("user_notifications",        lambda d: d["user"]["notifications"]),
+            ("user_listed_count",         lambda d: d["user"]["listed_count"]),
+            ("user_created_at",           lambda d: d["user"]["created_at"]),
+            ("user_name",                 lambda d: d["user"]["name"]),
+            ("user_following",            lambda d: d["user"]["following"]),
+            ("retweet_count",             lambda d: d["retweet_count"]),
+            ("text",                      lambda d: d["text"]),
+            ("entities_hashtags",         lambda d: d["entities"]["hashtags"]),
+            ("entities_urls",             lambda d: d["entities"]["urls"]),
+            ("entities_user_mentions",    lambda d: d["entities"]["user_mentions"]),
+            ("favorited",                 lambda d: d["favorited"]),
+            ("created_at",                lambda d: d["created_at"]),
+            ("contributors",              lambda d: d["contributors"])]
+HEADERS = zip(*FEATURES)[0]
+PATHS   = zip(*FEATURES)[1]
 
 def read_json(filename, gz=None):
     """
@@ -67,7 +71,7 @@ def read_json(filename, gz=None):
                 if parse_fails and not parse_fails % 100:
                     print ("WARNING: {0} json object " +
                            "parse failures ({1})").format(parse_fails,
-                                                           ex.message)
+                                                          ex.message)
             if js:
                 data.append(js)
 
@@ -84,7 +88,7 @@ def csvify_json_obj(jobj):
     """
     Return the json object ``jobj`` flattened into a line of csv
     """
-    return map(lambda p: p(jobj), FEATURES.values())
+    return [p(jobj) for p in PATHS]
 
 def process_json(filename, output_filename, gz=False):
     """
@@ -102,7 +106,7 @@ def process_json(filename, output_filename, gz=False):
 
     with open(output_filename, 'wb') as fout:
         tweet_writer = csv.writer(fout)
-        tweet_writer.writerow(FEATURES.keys()) # csv header
+        tweet_writer.writerow(HEADERS) # csv header
         print "Reading '{}'...".format(f)
         js = read_json(filename, gz=gz)
         print "Processing...".format(f)
