39b578ec141da761f9301c76c22a2e9d3d686773
dwcoates <dodge.w.coates@gmail.com>
2017-03-19 01:15:42 -0400

    Remove twits.org, to be replaced by readme.md

--- src/process.py
+++ src/process.py
@@ -2,7 +2,7 @@ from __future__ import division
 
 from dateutil import parser
 import sys
-from time import time
+import time
 
 import pandas as pd
 import numpy as np
@@ -13,12 +13,12 @@ def process_file(filename):
     start = time.time()
     sys.stdout.write("Reading...\r")
     df = pd.read_csv(filename)
-    print "Time to read: {:,} seconds".format(time.time() - start)
+    print "Time to read: {:,.2f} seconds".format(time.time() - start)
 
     start = time.time()
     sys.stdout.write("Converting to datetime...\r")
     df.created_at = df.created_at.apply(lambda x: parser.parse(x))
-    print "Time to read: {:,} minutes".format((time.time() - start) / 60)
+    print "Time to read: {:,.2f} minutes".format((time.time() - start) / 60)
 
     print "Done."
 
