2b5582b930a3d4ce24f0676c390047df02086efa
dwcoates <dodge.w.coates@gmail.com>
2017-03-19 04:13:38 -0400

    correct preprocess standardization

--- src/process.py
+++ src/process.py
@@ -7,19 +7,65 @@ import time
 import pandas as pd
 import numpy as np
 
-def process_file(filename):
-    sys.stdout.flush()
-    sys.stdout.write("Processing '{}'\r".format(filename))
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+print "{:,.2f}".format((df.retweet_count[df.retweet_count.apply(
+    lambda x: "+" in str(x))].size / df.retweet_count.size * 100))
+
+def process_date_time(df):
+    # Date/Time
+    start = time.time()
+    sys.stdout.write("Converting to datetime...\r")
+    df.created_at = df.created_at.apply(lambda x: parser.parse(x))
+    print "Time to process dates: {:,.2f} minutes".format((time.time() - start) / 60)
+
+    return df
+
+def read_csv(filename):
+    # Reading
     start = time.time()
     sys.stdout.write("Reading...\r")
     df = pd.read_csv(filename)
     print "Time to read: {:,.2f} seconds".format(time.time() - start)
 
-    start = time.time()
-    sys.stdout.write("Converting to datetime...\r")
-    df.created_at = df.created_at.apply(lambda x: parser.parse(x))
-    print "Time to read: {:,.2f} minutes".format((time.time() - start) / 60)
+    return df
 
-    print "Done."
+def process_retweet_count(df):
+    start = time.time()
+    sys.stdout.write("Dropping bad retweet data...\r")
+    df = df[df.retweet_count.apply(lambda x: "+" not in str(x))]
+    print "Time to drop bad data in retweets: {:,.2f} minutes".format((time.time() - start) / 60)
+
+    return df
+
+def standardize_counts(df):
+    start = time.time()
+    sys.stdout.write("Standardizing measures...\r")
+    minscaler = MinMaxScaler(feature_range=(-1,1))
+    stdscaler = StandardScaler()
+    columns   = ["user_friends_count",
+                 "user_favourites_count",
+                 "user_statuses_count",
+                 "user_followers_count",
+                 "user_listed_count",
+                 "retweet_count"]
+
+    df[columns] = df[columns].apply(lambda c: stdscaler.fit_transform(pd.to_numeric(c)))
+
+    print "Time to standardize: {:,.2f} minutes".format((time.time() - start) / 60)
+
+    return df
+
+def process_file(filename):
+    print "Processing '{}'\r".format(filename)
+    start = time.time()
+
+    df = read_csv(filename)
+    df = process_date_time(df)
+    df = process_retweet_count(df)
+    df = standardize_counts(df)
+
+    print "Finished processing '{}' after {:,.2f} minutes".format(filename,
+                                                                  time.time() - start)
 
     return df
