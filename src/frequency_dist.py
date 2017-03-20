9ff4eb42f275aaf7d232b2b44a66d827688135db
dwcoates <dodge.w.coates@gmail.com>
2017-03-19 21:13:44 -0400

    Use FreqDist even though it's really, really slow

--- src/frequency_dist.py
+++ src/frequency_dist.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import os
 from time import time
 import sys
@@ -6,56 +8,69 @@ import unicodecsv as csv
 from collections import defaultdict
 
 import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
 
-def get_text_word_freqs(df, frequencies=defaultdict(int)):
-    start = time()
+def progress_bar(value, endvalue, bar_length=20):
+    percent = float(value) / endvalue
+    arrow = '-' * int(round(percent * bar_length)-1) + '>'
+    spaces = ' ' * (bar_length - len(arrow))
 
+    sys.stdout.write("\rPercent: [{0}] {1}%".format(arrow + spaces,
+                                                    int(round(percent * 100))))
     sys.stdout.flush()
-    sys.stdout.write("Building corpus...\r")
+
+def get_text_word_freqs(df, frequencies):
     content = " ".join(df.text).decode("utf8")
 
-    sys.stdout.flush()
-    sys.stdout.write("Tokenizing words...\r")
-    words = content.split(" ") # vastly faster than ntlk.word_tokenize
+    #words = content.split(" ") # vastly faster than ntlk.word_tokenize
+    # punctuation = [".", "," ";", "!"]
+    # words = [w for w in words if len(w) > 2 and "#" != w[0]]
+    # words = [w for w in words if len(w) > 2 and "@" != w[0]]
+    # words = [w[:-1] if len(w) >= 2 and w[-1] in punctuation else w for w in words]
+    # for word in words:
+    #     frequencies[word] += 1
 
-    sys.stdout.flush()
-    sys.stdout.write("Dropping hashtags...\r")
-    words = [w for w in words if len(w) > 2 and "#" in w]
-
-    sys.stdout.flush()
-    sys.stdout.write("Accumulating frequencies...\r")
-    for word in words:
-        frequencies[word] += 1
-
-    msg = "Time to produce frequencies for corpus of {:,} words: {:.2f} seconds"
-    print msg.format(len(words), time() - start)
+    freqs = FreqDist(word_tokenize(content))
+    for word, count in freqs.iteritems():
+        frequencies[word] += count
 
     return frequencies
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         raise ValueError(
-            "{} accepts 2 arguments, recieved {}".format(
-                __file__,
-                len(sys.argv) - 1))
+            "{} accepts 2 arguments, recieved {}".format(__file__,
+                                                         len(sys.argv) - 1))
 
     DATA_PATH = os.path.abspath(sys.argv[1])
-    OUTFILENAME = os.path.abspath(sys.argv[2])
-    OUTFILE = os.join(os.path.abspath(DATA_PATH),
-                      os.path.(OUTFILENAME))
+    OUTFILENAME = os.path.basename(sys.argv[2])
+    TEST_LIMIT = int(sys.argv[3]) if len(sys.argv) >= 4 else None
+    OUTFILE = os.path.join(os.path.dirname(DATA_PATH),
+                           OUTFILENAME)
     FILES = os.listdir(DATA_PATH)
+    if TEST_LIMIT:
+        FILES = np.random.choice(FILES, TEST_LIMIT)
 
-    print "Determining word counts for {} files in '{}':\n".format(len(FILES),
+    print "Determining word counts for {} files in '{}':".format(len(FILES),
                                                                    DATA_PATH)
+    start = time()
     freqs = defaultdict(int)
-    for file in FILES:
-        freqs = get_text_word_freqs(file)
+    for i, file in enumerate(FILES):
+        progress_bar(i, len(FILES), 50)
+        df = pd.read_csv(os.path.join(DATA_PATH, file), low_memory=False)
+        freqs = get_text_word_freqs(df, freqs)
+        progress_bar(i, len(FILES), 50)
+    msg = "\nTime to produce frequencies for all files: {:,.2f} minutes"
+    print msg.format(len(FILES), (time() - start) / 60.0)
 
     print "Writing to outfile '{}'".format(OUTFILE)
-    dictionary_writer = csv.writer(OUTFILE)
-    dictionary = zip(*freqs.keys(), *freqs.values())
-    dictionary_writer.writerow(["word", "count"])
-    for d in dictionary:
-        dictionary_writer.writerow(d)
+    with open(OUTFILE, 'wb') as fout:
+        dictionary_writer = csv.writer(fout)
+        dictionary = zip(freqs.keys(), freqs.values())
+        dictionary_writer.writerow(["word", "count"])
+        for d in dictionary:
+            dictionary_writer.writerow(d)
 
     print "Done."
