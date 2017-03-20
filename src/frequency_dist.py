81f8cc1a58544f0d1bef3a05a056ff82e6e691ec
dwcoates <dodge.w.coates@gmail.com>
2017-03-19 23:16:15 -0400

    Add error count

--- src/frequency_dist.py
+++ src/frequency_dist.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
+from __future__ import division
 
 import os
 from time import time
 import sys
+import warnings
 
 import unicodecsv as csv
 from collections import defaultdict
@@ -12,31 +14,49 @@ import numpy as np
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
 
+
+type_errors=0
+
 def progress_bar(value, endvalue, bar_length=20):
     percent = float(value) / endvalue
     arrow = '-' * int(round(percent * bar_length)-1) + '>'
     spaces = ' ' * (bar_length - len(arrow))
 
-    sys.stdout.write("\rPercent: [{0}] {1}%".format(arrow + spaces,
-                                                    int(round(percent * 100))))
+    sys.stdout.write(
+        "\rPercent: [{0}] {1}% | type errors: {2:,}".format(arrow + spaces,
+                                                           int(round(percent * 100)),
+                                                           type_errors))
     sys.stdout.flush()
 
 def get_text_word_freqs(df, frequencies):
-    content = " ".join(df.text).decode("utf8")
+    global type_errors
 
-    #words = content.split(" ") # vastly faster than ntlk.word_tokenize
-    # punctuation = [".", "," ";", "!"]
-    # words = [w for w in words if len(w) > 2 and "#" != w[0]]
-    # words = [w for w in words if len(w) > 2 and "@" != w[0]]
-    # words = [w[:-1] if len(w) >= 2 and w[-1] in punctuation else w for w in words]
-    # for word in words:
-    #     frequencies[word] += 1
+    try:
+        content = " ".join([str(s) for s in df.text]).decode("utf8")
+    except:
+        type_errors += 1
+        raise ValueError(
+            "Text value in a dataframe can't be coerced to string.")
 
     freqs = FreqDist(word_tokenize(content))
     for word, count in freqs.iteritems():
         frequencies[word] += count
 
     return frequencies
+
+def compute_word_diversities(df, freqs):
+    def word_diversity(t):
+        """
+        Get average word popularity for the given message
+        """
+        ws = word_tokenize(t)
+        if len(ws) != 0:
+            return sum([freqs[w] for w in ws]) / len(ws)
+        else:
+            return 0
+
+    fs = df.text.apply(word_diversity)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -59,8 +79,13 @@ if __name__ == "__main__":
     freqs = defaultdict(int)
     for i, file in enumerate(FILES):
         progress_bar(i, len(FILES), 50)
-        df = pd.read_csv(os.path.join(DATA_PATH, file), low_memory=False)
-        freqs = get_text_word_freqs(df, freqs)
+        df = pd.read_csv(os.path.join(DATA_PATH, file))
+        try:
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore",category=DeprecationWarning)
+                freqs = get_text_word_freqs(df, freqs)
+        except ValueError as ex:
+            print ex.message
         progress_bar(i, len(FILES), 50)
     msg = "\nTime to produce frequencies for all files: {:,.2f} minutes"
     print msg.format(len(FILES), (time() - start) / 60.0)
@@ -71,6 +96,9 @@ if __name__ == "__main__":
         dictionary = zip(freqs.keys(), freqs.values())
         dictionary_writer.writerow(["word", "count"])
         for d in dictionary:
-            dictionary_writer.writerow(d)
+            try:
+                dictionary_writer.writerow(d)
+            except Exception as ex:
+                print "Error while writing '{}': {}".format(d, ex.message)
 
     print "Done."
