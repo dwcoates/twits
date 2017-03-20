962d19a0f8fd096cbcbfaff6b3ba28f4c2fde9a8
dwcoates <dodge.w.coates@gmail.com>
2017-03-20 00:36:37 -0400

    Drop useless columns, get timestamps

--- src/process.py
+++ src/process.py
@@ -4,6 +4,7 @@ from dateutil import parser
 import sys
 import time
 import warnings
+from datetime import datetime
 
 import pandas as pd
 import numpy as np
@@ -12,10 +13,16 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
 def process_date_time(df):
+    def parse(ts):
+        ret = ts.split(" ")
+        ret.pop(4)
+        return datetime.strptime(
+            parse(" ".join(ret), '%a %b %d %X %Y')
+
     # Date/Time
     start = time.time()
     sys.stdout.write("Converting to datetime...\r")
-    df.created_at = df.created_at.apply(lambda x: parser.parse(x))
+    df.created_at = df.created_at.apply(parse)
     print "Time to process dates: {:,.2f} minutes".format((time.time() - start) / 60)
 
     return df
@@ -57,14 +64,39 @@ def standardize_counts(df):
 
     return df
 
+def drop_extra_columns(df):
+    """
+    Drop useless columns that shouldn't have made it past preprocessing
+    """
+    columns = ["created_at.1",
+               "user_lang",
+               "user_notifications",
+               "user_verified",
+               "user_following",
+               "retweeted",
+               "favorited",
+               "contributors"]
+
+    return df.drop(columns, axis=1)
+
+def add_y(df):
+    df.retweeted()
+
+    return df
+
+def process_df(df):
+    df = process_date_time(df)
+    df = process_retweet_count(df)
+    df = standardize_counts(df)
+    df = drop_extra_columns(df)
+
+    return df
+
 def process_file(filename):
     print "Processing '{}'\r".format(filename)
     start = time.time()
 
-    df = read_csv(filename)
-    df = process_date_time(df)
-    df = process_retweet_count(df)
-    df = standardize_counts(df)
+    df = process_df(read_csv(filename))
 
     print "Finished processing '{}' after {:,.2f} minutes".format(filename,
                                                                   (time.time() - start) / 60)
