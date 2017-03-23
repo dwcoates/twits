570e8a584afc91815fd038c31f481a2b7d9c3435
dwcoates <dodge.w.coates@gmail.com>
2017-03-23 17:54:04 -0400

    Test feature normalization

--- develop/data.py
+++ develop/data.py
@@ -3,6 +3,12 @@ from __future__ import division
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+from src import core
+from src import process
+from src import featurize
 
 # don't use scientific notation when printing
 np.set_printoptions(suppress=True)
@@ -25,4 +31,16 @@ print f_count
 # There's an interesting dip in the early amounts of
 # friendship.
 plt.hist(f_count, bins=100)
+plt.show()
+
+
+
+df = core.read_csv("data/toy_sample_tweets.csv")
+df = df[featurize.CORE_FEATURES]
+df = df.dropna()
+df = process.process_retweet_count(df)
+df = process.compute_and_add_target(df)
+
+for c in reload(process).standardize_counts(df).columns:
+    sns.kdeplot(df[c])
 plt.show()
