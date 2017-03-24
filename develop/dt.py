74346532a2f37de6273a193d700759c941db30cc
dwcoates <dodge.w.coates@gmail.com>
2017-03-24 03:11:42 -0400

    get dt working

--- develop/dt.py
+++ develop/dt.py
@@ -3,31 +3,37 @@ from __future__ import division
 import time
 
 import numpy as np
-from sklearn.tree import DecisionTreeRegressor
+from sklearn import tree
 import matplotlib.pyplot as plt
 
 from src import featurize
+from src import core
+from src import process
 
 import pandas as pd
 
 # this rU flag is some fix I found at https://github.com/pandas-dev/pandas/issues/11166
-df = pd.read_csv(open("../data/processed_sample_tweets.csv", 'rU'), encoding="utf8")
+df = core.read_csv("data/processed_toy_sample_tweets.csv")
 
-fname = "data/processed_toy_sample_tweets_X_train.csv"
-print "Reading features from '{}'...".format(fname)
-X = pd.read_csv(open(fname, 'rU'), encoding="utf8")
-
-fname = "data/processed_toy_sample_tweets_y_train.csv"
-print "Reading target from  '{}'...".format(fname)
-y = pd.read_csv(open(fname, 'rU'), encoding="utf8")
+X_train = core.read_csv("data/processed_toy_sample_tweets_X_train.csv")
+y_train = core.read_csv("data/processed_toy_sample_tweets_y_train.csv")
+X_test = core.read_csv("data/processed_toy_sample_tweets_X_test.csv")
+y_test = core.read_csv("data/processed_toy_sample_tweets_y_test.csv")
 
 #
 # off the cuff feature dropping
 #
-X = X[featurize.CORE_FEATURES]
+X_train = X_train[featurize.CORE_FEATURES]
+X_test = X_test[featurize.CORE_FEATURES]
 
-clf = DecisionTreeRegressor(X, y)
+
+clf = tree.DecisionTreeRegressor()
 start = time.time()
 print "Fitting decision tree..."
-clf.fit(X, y)
+clf.fit(X_train, y_train)
 print "Time to fit dt: {} minutes".format((start - time.time()) / 60)
+
+outfile = tree.export_graphviz(clf, out_file='filename.dot',
+                               feature_names=X.columns)
+
+outfile.close()
